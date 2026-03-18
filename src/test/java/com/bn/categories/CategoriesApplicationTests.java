9d6708cb90d8f89923d1af71683d21f4b6579e5a
Camila Reinke <reinkecamila@gmail.com>
2026-03-18 16:50:03 -0300

    build: adiciona estrutura do projeto

---
+++ src/test/java/com/bn/categories/CategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
