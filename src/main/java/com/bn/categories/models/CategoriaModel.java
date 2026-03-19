c5c00cc8019eae1b6fc7fc10b917e40df9b0c16f
Camila Reinke <reinkecamila@gmail.com>
2026-03-19 14:33:30 -0300

    feat: cria entidade Categoria

---
+++ src/main/java/com/bn/categories/models/CategoriaModel.java
@@ -0,0 +1,40 @@
+package com.bn.categories.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_categoria")
+    private String nome;
+
+    @Column(name = "descricao_categoria")
+    private String descricao;
+
+    public CategoriaModel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
