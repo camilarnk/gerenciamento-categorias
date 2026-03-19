e760cc951a4265a543248e4c48f37eae9ab6bc79
Camila Reinke <reinkecamila@gmail.com>
2026-03-19 15:54:10 -0300

    feat: cria controlador REST

---
+++ src/main/java/com/bn/categories/controllers/CategoriaController.java
@@ -0,0 +1,48 @@
+package com.bn.categories.controllers;
+
+import com.bn.categories.models.CategoriaModel;
+import com.bn.categories.services.CategoriaService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @PostMapping
+    public ResponseEntity<CategoriaModel> criarCategoria(@RequestBody CategoriaModel categoriaModel) {
+        CategoriaModel request = categoriaService.criarCategoria(categoriaModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(request.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaModel>> buscarTodasCategorias() {
+        List<CategoriaModel> request = categoriaService.buscarTodasCategorias();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<CategoriaModel>> buscarPorId(@PathVariable Long id) {
+        Optional<CategoriaModel> request = categoriaService.buscarPorId(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarCategoria(@PathVariable Long id) {
+        categoriaService.deletarCategoria(id);
+        return ResponseEntity.noContent().build();
+    }
+}
